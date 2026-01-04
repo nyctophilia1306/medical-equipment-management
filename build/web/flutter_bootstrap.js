badde8699081414cecde69baee5102361d2cde73
DDP16 <phat16122004@gmail.com>
2026-01-04 21:31:39 +0700

    Add web build with working logo

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3941467190"
+    serviceWorkerVersion: "1902557317"
   }
 });
