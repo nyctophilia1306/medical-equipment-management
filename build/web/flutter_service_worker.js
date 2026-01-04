02e5e1f9c8055b2d4522cdb97293efe38019de03
DDP16 <phat16122004@gmail.com>
2026-01-04 22:19:40 +0700

    Fix all flutter analyze issues: add curly braces and suppress Radio deprecation warnings

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "de7282444e267e493ed9ee129f920983
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3bca7a89e051848526a8a819695fa9f7",
+"flutter_bootstrap.js": "8013371019df5511f5f29e69a5a29697",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
