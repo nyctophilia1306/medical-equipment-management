b31cac7179a281a90ab3df4c1e00388b57dac080
DDP16 <phat16122004@gmail.com>
2026-01-16 05:37:51 +0700

    build: update web build with email field changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "551459611"
+    serviceWorkerVersion: "1673764858"
   }
 });
